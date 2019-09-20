5e0b6e2b554eff0c8c120fcd3b1934009065fec2
jzes <pauloneto.jose@gmail.com>
2019-09-20 00:30:55 -0300

    get answer html ok

--- main.js
+++ main.js
@@ -2,22 +2,11 @@ const robot = require('./robot')
 const parser = require('./parser')
 
 async function main(){
-    let tokenHTML = `<html>
-    <head>
-        <script src="adpagespeed.js"></script>
-    </head>
-    <body>
-    <form action="/" method="post" id="form">
-        <input type="hidden" name="token" id="token" value="xv6z7912u04yx26vz1046uxx07vz7yz3" />
-        <input type="button" value="Descobrir resposta" onClick="findAnswer()">
-    </form>
-    </body>
-    </html>
-    `//await robot.getTokenHTML()
-    let encryptedToken = parser.encryptToken(tokenHTML)
-    console.log(encryptedToken);
-    // let answerHTML = robot.fetchAnswerHTML(encryptedToken)
-    // let answer = parser.parseAnswerHTML(answerHTML)
+    let tokenHTML = await robot.getTokenHTML()
+    let encryptedToken = parser.getEncryptedTokenFromHTML(tokenHTML)
+    let answerHTML = await robot.fetchAnswerHTML(encryptedToken)
+    console.log(answerHTML);
+    //let answer = parser.parseAnswerHTML(answerHTML)
     //console.log(answer)
 }
 
--- robot.js
+++ robot.js
@@ -1,17 +1,65 @@
-const reqPromise = require('request-promise')
+const reqPromise = require('request-promise').defaults({ jar: true });
 
 const mainURL = "http://applicant-test.us-east-1.elasticbeanstalk.com/"
+// Put cookie in an jar which can be used across multiple requests
+let PHPSessionID
 
-async function getTokenHTML(){
-    try{
-        let html = await reqPromise(mainURL)
-        return html
-    }catch{
-        console.log('Token request cannot be completed')
+async function getTokenHTML() {
+
+    let options = {
+        method: 'GET',
+        uri: mainURL,
+        resolveWithFullResponse: true
+    };
+
+    try {
+        let response = await reqPromise(options)
+        fillPHPSessionID(response)
+        return response.body
+    } catch (err) {
+        console.log('Token request cannot be completed\n', err)
         process.abort()
     }
 }
 
+function fillPHPSessionID(response){
+    PHPSessionID = response.headers['set-cookie']
+    .filter(cookie => {
+        return cookie.includes('PHPSESSID')
+    })[0].split(';')[0]
+}
+
+async function fetchAnswerHTML(encryptedToken) {
+    let options = {
+        method: 'POST',
+        uri: mainURL,
+        headers:
+        {
+            'cache-control': 'no-cache',
+            Cookie: PHPSessionID,
+            'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6',
+            'Accept-Encoding': 'gzip, deflate',
+            Referer: 'http://applicant-test.us-east-1.elasticbeanstalk.com/',
+            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Upgrade-Insecure-Requests': '1',
+            Origin: 'http://applicant-test.us-east-1.elasticbeanstalk.com',
+            'Cache-Control': 'max-age=0',
+            Connection: 'keep-alive'
+        },
+        form:
+        {
+            token: encryptedToken,
+            undefined: undefined
+        }
+    }
+
+    let html = await reqPromise(options)
+    return html
+}
+
 module.exports = {
-    getTokenHTML: getTokenHTML
+    getTokenHTML: getTokenHTML,
+    fetchAnswerHTML: fetchAnswerHTML
 }
